30aca384f1f830905a5bcb1fdf9ff7a40ae29597
peruyeroj <peruyero@udel.edu>
2023-10-03 19:21:42 -0400

    Fixed another lint issue

--- src/components/RevealAnswer.tsx
+++ src/components/RevealAnswer.tsx
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import { Button } from "react-bootstrap";
 
 export function RevealAnswer(): JSX.Element {
     const [isVisible, setIsVisible] = useState(false);
